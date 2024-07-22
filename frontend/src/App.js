cc8d7494ebc223ad722fb28a4c58db531adc19a8
AKflower <anhkhoabqv@gmail.com>
2024-07-23 01:45:10 +0700

    [Feat] Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/navbar/navbar';
 import { BrowserRouter, Route, Link, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Header from './components/header/header';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home />} />
-
+    <Route path='/admin/login' element={<Login />} />
     </Routes>
     </BrowserRouter>
   );
