304f292703a243aca559ce76884dcb07b48f06d7
AKflower <anhkhoabqv@gmail.com>
2024-07-28 12:38:58 +0700

    [Feat]

---
+++ frontend/src/pages/Dashboard copy/dashboard.js
@@ -0,0 +1,9 @@
+import styles from './dashboard.module.scss'
+
+export default function Dashboard() {
+    return (
+        <div className='main'>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
